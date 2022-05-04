3c864abd79c8c2374b8345faebfcc79ec58fcba9
Dmitriev R.R <education.romichdmitriev@gmail.com>
2022-05-04 14:27:50 +0500

    feat: add lint, formattiing, ci/cd tools - add prettier, prettier config - add es-lint airbnb typescript config - add stylelint, stylelint config - add husky and lint-staged pre-commit hook - add github actions for linting

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest', 'airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    files: ['*.ts', '*.tsx'],
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      legacyDecorators: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-unused-vars': 1,
+        '@typescript-eslint/no-shadow': 'off',
+        'react/prop-types': 0,
+        'react/require-default-props': 0,
+        'import/extensions': 0,
+        'import/order': 0,
+        'react/function-component-definition': [
+          0,
+          {
+            namedComponents: 'function-declaration',
+          },
+        ],
+      },
+    },
+  ],
+};
